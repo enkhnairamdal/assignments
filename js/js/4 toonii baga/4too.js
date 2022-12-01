9148c5ae3ec596cea7ff976513c5583dade2e674
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2022-12-01 19:17:02 +0800

    4 too

---
+++ js/js/4 toonii baga/4too.js
@@ -0,0 +1,17 @@
+    let a = 120
+    let b = 33
+    let c = 10 
+    let d = 12
+    if ( a <= 10 ){
+        console.log(a)
+    }
+    if ( b <=10 ){
+        console.log(b)
+    }
+    if ( c <=10 ){
+        console.log(c)
+    }
+
+    if ( d <= 10 ){
+        console.log(d)
+    }
