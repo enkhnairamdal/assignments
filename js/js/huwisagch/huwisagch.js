affe6a9d92d0c7342b68c3796cf710304d6340e4
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2022-12-01 19:38:21 +0800

    huwisagch

---
+++ js/js/huwisagch/huwisagch.js
@@ -0,0 +1,23 @@
+    let a = 85
+    let b = 75
+    let c = 96
+    let d = 69 
+    let niilber = 0
+    if( a > 80 )
+    {
+        niilber = niilber + a
+    
+    }
+    if ( b > 80)
+    {
+        niilber = niilber + b
+    }
+    if ( c > 80)
+    { 
+        niilber = niilber + c
+    }
+    if ( d > 80)
+    { 
+        niilber = niilber + d
+    }
+    console.log(niilber)
