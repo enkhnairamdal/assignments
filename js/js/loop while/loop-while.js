ac106277b8bd75df589f8522235605be2003ea16
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2022-12-06 20:40:10 +0800

    --

---
+++ js/js/loop while/loop-while.js
@@ -0,0 +1,55 @@
+//1
+    // let num = 10
+    // for ( let a= 1 ; a <= num; a++)
+    // {console.log(a)}
+//--------->
+//2
+    // let sum = 0
+    // for ( let i = 1; i <= 10; i++ )
+    // {
+    //     if( i % 2 !== 0)
+    //     console.log(i)
+            
+    // }
+//------->
+//3
+    // for ( let i =1; i <= 10; i++)
+    // {
+    //     if ( i % 2 == 0)
+    //     console.log(i)
+    // }
+//----------->
+//4
+    // let total = 0
+    // for ( i = 1; i <= 100; i++ )
+    // {
+    //     total = total + i 
+    // }
+    // console.log(total)
+//5
+    // let s = prompt('Number?')
+    // let total = 0
+    // for ( i = 1; i <= s; i++ )
+    // {
+    //     total = total + i 
+    // }
+    // console.log(total)
+//----------->
+//6
+   let num = prompt('Number ?')
+   for (let i = 1 ; i <= num; i++){
+ 
+ 
+   
+   
+
+ 
+
+
+    
+
+
+
+
+
+
